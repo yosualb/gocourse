1f8deab0a9cb23b34c09fcfcc9e8a235bfdc2c06
yosualb <yos@yosualb.com>
2016-12-10 18:46:58 +0700

    Add variable lesson

---
+++ variable/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Shorthand variable
+	i := 1
+
+	// Zero value variable
+	var j int
+
+	// Initialized variable
+	var k int = 10
+
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+}
